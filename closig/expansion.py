d06f1d10a54d029f690089c4d522f28c5da18760
Simon Zwieback <szwieback@alaska.edu>
2023-04-26 11:45:37 -0800

    notation: p for scene

--- closig/expansion.py
+++ closig/expansion.py
@@ -9,31 +9,31 @@ from abc import ABC, abstractmethod
 from pip._vendor.pyparsing.core import Forward
 
 # Covariance:
-# ..., N, N
+# ..., P, P
 
 class Basis():
-    def __init__(self, N):
-        self.N = N
+    def __init__(self, P):
+        self.P = P
         self._initalize()
 
     @abstractmethod
     def _initialize(self):
         pass
 
-    def _std_basis(self, n0, n1):
-        if max(n0, n1) >= self.N or min(n0, n1) < 0 or n0 >= n1: raise ValueError(f"{n0}, {n1} not valid")
-        ind = n0 * self.N - n0 * (n0 + 1) // 2 + (n1 - n0 - 1)
+    def _std_basis(self, p0, p1):
+        if max(p0, p1) >= self.P or min(p0, p1) < 0 or p0 >= p1: raise ValueError(f"{p0}, {p1} not valid")
+        ind = p0 * self.P - p0 * (p0 + 1) // 2 + (p1 - p0 - 1)
         return ind
     
-    def _std_basis_vec(self, n0, n1):
+    def _std_basis_vec(self, p0, p1):
         # includes self-connected edges (for "intensities")
-        if max(n0, n1) >= self.N or min(n0, n1) < 0 or n0 > n1: raise ValueError(f"{n0}, {n1} not valid")
-        ind = n0 * self.N - n0 * (n0 - 1) // 2 + (n1 - n0)
+        if max(p0, p1) >= self.P or min(p0, p1) < 0 or p0 > p1: raise ValueError(f"{p0}, {p1} not valid")
+        ind = p0 * self.P - p0 * (p0 - 1) // 2 + (p1 - p0)
         return ind
 
     # get covector in standard basis
     def to_std_basis(self, covector):
-        xi = np.zeros(self.N * (self.N - 1) // 2, dtype=np.int16)
+        xi = np.zeros(self.P * (self.P - 1) // 2, dtype=np.int16)
         for edge in covector:
             xi[self._std_basis(edge[0], edge[1])] = edge[2]
         return xi
@@ -92,7 +92,7 @@ class Basis():
             return np.array(gcohs)
 
     def _test_number(self):
-        assert len(self.covectors) == (self.N - 1) * (self.N - 2) / 2
+        assert len(self.covectors) == (self.P - 1) * (self.P - 2) / 2
 
     def _test_dimension(self):
         from scipy.linalg import lu
@@ -101,91 +101,91 @@ class Basis():
         assert np.min(np.abs(np.diag(U))) > 1e-16
 
 class SmallStepBasis(Basis):
-    def __init__(self, N):
-        self.N = N
+    def __init__(self, P):
+        self.P = P
         self._initialize()
 
     def _initialize(self):
         intervals, covectors = [], []
-        nt, ntau = [], []
+        pt, ptau = [], []
         # assembly
-        for nb in range(self.N - 2):
-            for ne in range(nb + 2, self.N):
-                intervals.append((nb, ne))
-                covectors.append(self._covector(nb, ne))
-                nt.append(self._nt(nb, ne))
-                ntau.append(self._ntau(nb, ne))
+        for pb in range(self.P - 2):
+            for pe in range(pb + 2, self.P):
+                intervals.append((pb, pe))
+                covectors.append(self._covector(pb, pe))
+                pt.append(self.pt(pb, pe))
+                ptau.append(self._ptau(pb, pe))
         # sorting
-        lsort = lambda x: [x[ind] for ind in np.lexsort((nt, ntau))]
-        self.ntau = np.array(lsort(ntau))
-        self.nt = np.array(lsort(nt))
+        lsort = lambda x: [x[ind] for ind in np.lexsort((pt, ptau))]
+        self.ptau = np.array(lsort(ptau))
+        self.pt = np.array(lsort(pt))
         self.covectors = lsort(covectors)
         self.intervals = np.array(lsort(intervals))
 
     @staticmethod
-    def _nt(nb, ne):
-        return 0.5 * (nb + ne)
+    def pt(pb, pe):
+        return 0.5 * (pb + pe)
 
     @staticmethod
-    def _ntau(nb, ne):
-        return ne - nb
+    def _ptau(pb, pe):
+        return pe - pb
 
     @staticmethod
-    def _covector(nb, ne):
-        return [(n, n + 1, 1) for n in range(nb, ne)] + [(nb, ne, -1)]
+    def _covector(pb, pe):
+        return [(p, p + 1, 1) for p in range(pb, pe)] + [(pb, pe, -1)]
 
 # class for two hop
 # loop over t, tau
 
 class TwoHopBasis(Basis):
-    def __init__(self, N):
-        self.N = N
+    def __init__(self, P):
+        self.P = P
         self._initialize()
 
     def _initialize(self):
         self.covectors = []
-        intervals, nt, ntau = [], [], []
+        intervals, pt, ptau = [], [], []
         # assembly
-        for _ntau in range(2, self.N):
-            hminus, hplus = self._h(_ntau)
-            for _ntraw in range(hminus, self.N - hplus):
-                intervals.append((_ntraw - hminus, _ntraw + hplus))
-                self.covectors.append(self._covector(_ntraw, _ntau))
-                _nt = _ntraw + 0.5 * (hplus - hminus)
-                nt.append(_nt)
-                ntau.append(_ntau)
-        self.ntau, self.nt = np.array(ntau), np.array(nt)
+        for _ptau in range(2, self.P):
+            hminus, hplus = self._h(_ptau)
+            for _praw in range(hminus, self.P - hplus):
+                intervals.append((_praw - hminus, _praw + hplus))
+                self.covectors.append(self._covector(_praw, _ptau))
+                _pt = _praw + 0.5 * (hplus - hminus)
+                pt.append(_pt)
+                ptau.append(_ptau)
+        self.ptau, self.pt = np.array(ptau), np.array(pt)
         self.intervals = np.array(intervals)
 
     @staticmethod
-    def _h(ntau):
+    def _h(ptau):
         from math import floor, ceil
-        return floor(ntau / 2), ceil(ntau / 2)
+        return floor(ptau / 2), ceil(ptau / 2)
 
     @staticmethod
-    def _covector(nt, ntau):
-        hminus, hplus = TwoHopBasis._h(ntau)
-        return [(nt - hminus, nt, 1), (nt, nt + hplus, 1), (nt - hminus, nt + hplus, -1)]
+    def _covector(pt, ptau):
+        hminus, hplus = TwoHopBasis._h(ptau)
+        return [(pt - hminus, pt, 1), (pt, pt + hplus, 1), (pt - hminus, pt + hplus, -1)]
 
 if __name__ == '__main__':
     L = 40
-    N = 4
+    P = 4
     S = 20
-    # y = np.random.normal(size=(S, L, N)) + 1j * np.random.normal(size=(S, L, N))
+    # y = np.random.normal(size=(S, L, P)) + 1j * np.random.normal(size=(S, L, P))
     # C = np.mean(y[..., np.newaxis] * y[..., np.newaxis,:].conj(), axis=-3)
     # print(C.shape)
-    # ss_covectors(N)
-    # for N in range(3, 5):
-    #     b = TwoHopBasis(N)
-    #     print(N, len(b.covectors))
+    # ss_covectors(P)
+    # for P in range(3, 5):
+    #     b = TwoHopBasis(P)
+    #     print(P, len(b.covectors))
     #     b._test_dimension()
     #     b._test_number()
 
-    N = 91
-    b = TwoHopBasis(N)
-    for n0 in range(N):
-        for n1 in range(n0, N):
-            print(n0, n1, b._std_basis_vec(n0, n1))
-    print(N * (N+1) / 2)
+    P = 91
+    b = TwoHopBasis(P)
+    for p0 in range(P):
+        for p1 in range(p0, P):
+            print(p0, p1, b._std_basis_vec(p0, p1))
+    print(P * (P+1) / 2)
     
     # std_basis_Cvec
