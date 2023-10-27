c8de898eb798d386dcaa80cc006afc0779aaafa1
Simon Zwieback <szwieback@alaska.edu>
2023-10-27 11:29:29 -0800

    restructure

---
+++ closig/__init__.py
@@ -0,0 +1,4 @@
+from closig.expansion import TwoHopBasis, SmallStepBasis
+from closig.model import (
+    Geom, CovModel, LayeredCovModel, ContHetDispModel, Layer, TiledCovModel, HomogSoilLayer,
+    SeasonalVegLayer, PrecipScatterSoilLayer)
